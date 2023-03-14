8617893fd9ce999c91cee36bd49538d2056a576a
Antonin DUVAUCHEL <antonin.duvauchel.etu@bouleau09.iut-infobio.priv.univ-lille1.fr>
2023-03-14 14:22:05 +0100

    ajout d'une class circle pour mieux répartir le code

---
+++ src/Circle.js
@@ -0,0 +1,36 @@
+export class Circle{
+    constructor(x,y,score,color,context){
+        this.x=x;
+        this.y=y;
+        this.color=color;
+        this.score=score;
+        this.color=color
+        this.vitesse=vitesse;
+        this.context=context;
+    }
+    moveCircle(direction) {
+	    if(direction=="ArrowDown"){
+	    	this.y+=this.vitesse;
+	    }
+	    if(direction=="ArrowUp"){
+		    this.y-=this.vitesse;
+    	}
+	    if(direction=="ArrowLeft"){
+		    this.x-=this.vitesse;
+	    }
+	    if(direction=="ArrowRight"){
+		    this.x+=this.vitesse;
+	    }
+    }
+    drawCircle(){
+        this.context.beginPath();
+        this.context.fillStyle = ''+this.color;
+        this.context.lineWidth = 5;
+        this.context.arc(this.x, this.y, this.score, 0, 360, false);
+        this.context.stroke();
+        this.context.fill();
+    }
+    scoreVariation(){
+
+    }
+} 
