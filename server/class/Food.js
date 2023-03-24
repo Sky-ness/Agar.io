cc2c4bd4151f639746fdb87cd3f502773aee3e7e
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2023-03-24 23:35:32 +0100

    la classe player extends maintenant food , la gestion des couleur et des nom propres a chaque avatar est fait

---
+++ server/class/Food.js
@@ -0,0 +1,8 @@
+export class Food {
+	constructor(color, x, y, score) {
+		this.color = color;
+		this.x = x;
+		this.y = y;
+		this.score = score;
+	}
+}
--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ import { env } from 'node:process';
 import express from 'express';
 import { Server as IOServer } from 'socket.io';
 import addWebpackMiddleware from './utils/addWebpackMiddleware.js';
-import { Circle } from './class/Circle.js';
 import { generateRandomNumber } from './function/random.js';
 import { Maps } from './class/Maps.js';
+import { Player } from './class/Player.js';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -16,22 +16,30 @@ let mapS = new Maps(1000, 1000);
 mapS.randomFood(70, 80);
 
 io.on('connection', socket => {
-	console.log(`Nouvelle connexion du client ${socket.id}`);
+	let name = '' + socket.id;
+	let color;
+	socket.on('pseudo', pseudo => {
+		if (pseudo != '') {
+			name = pseudo;
+		}
+	});
+	socket.on('color', color1 => {
+		color = color1;
+	});
+
+	console.log(`Nouvelle connexion du client ${name}`);
 	socket.on('disconnect', () => {
-		// temporaire le temps que les mouvements ne fonctionne pas (ça déconnecte le joueur quand on ferme la page)
-		console.log(`Déconnexion du client ${socket.id}`);
-		// On supprime le joueur de la liste principale quand il se déconnecte
-		mapS.removePlayer(socket.id);
-		// On envoie les informations a tout les autres joueurs foods players restants et on nettoie le canva
+		console.log(`Déconnexion du client ${name}`);
+		mapS.removePlayer(name);
 		console.log(mapS.players);
-		io.emit('deconnexion');
 		io.emit('map', mapS);
 	});
 	// A la connection du joueur on créer un nouveau joueur sur le plateau
 	socket.on('play', () => {
 		mapS.addPlayer(
-			new Circle(
-				socket.id,
+			new Player(
+				name,
+				color,
 				generateRandomNumber(0, mapS.width),
 				generateRandomNumber(0, mapS.height),
 				20
